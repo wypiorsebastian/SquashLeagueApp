f937bcb31915a3c9c2f6e7ba53ab2b0ead2d76b8
Sebastian Wypiór <wypior.sebastian@gmail.com>
2022-03-14 06:32:17 +0100

    Add FlexLayout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MaterialModule} from "./material.module";
+import {FlexLayoutModule} from "@angular/flex-layout";
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material.module.ts
@@ -0,0 +1,18 @@
+import {NgModule} from "@angular/core";
+import {MatButtonModule} from "@angular/material/button";
+import {MatInputModule} from "@angular/material/input";
+import {MatIconModule} from "@angular/material/icon";
+
+@NgModule({
+  imports: [
+    MatButtonModule,
+    MatInputModule,
+    MatIconModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatInputModule,
+    MatIconModule
+  ]
+})
+export class MaterialModule {}
