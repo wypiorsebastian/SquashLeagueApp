6fb27d011e8a2592e8285bfefa53d8c630bff341
wypiorsebastian <wypior.sebastian@gmail.com>
2022-12-19 06:15:08 +0100

    Making Nav responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {AfterViewInit, Component, ViewChild} from '@angular/core';
 import {MatSidenav} from "@angular/material/sidenav";
-import {BreakpointObserver} from "@angular/cdk/layout";
+import {BreakpointObserver, Breakpoints} from "@angular/cdk/layout";
 import {MatDialog} from "@angular/material/dialog";
 import {openRegisterFormDialog} from "./auth/signup/signup.component";
 
@@ -11,21 +11,24 @@ import {openRegisterFormDialog} from "./auth/signup/signup.component";
 })
 export class AppComponent implements AfterViewInit{
   @ViewChild('sidenav') sidenav;
-
+  isHandset: boolean = false;
 
   constructor(private observer: BreakpointObserver,
               private dialog: MatDialog) {
   }
 
   ngAfterViewInit() {
-
-    this.observer.observe(['(max-width: 800px)']).subscribe((res) => {
+    this.sidenav.close();
+    //this.observer.observe(['(max-width: 800px)']).subscribe((res) => {
+    this.observer.observe(Breakpoints.Handset).subscribe((res) => {
       if (res.matches) {
+        this.isHandset = true;
         this.sidenav.mode = 'over';
         this.sidenav.close();
       } else {
+        this.isHandset = false;
         this.sidenav.mode = 'side';
-        this.sidenav.open();
+        //this.sidenav.open();
       }
     })
   }
